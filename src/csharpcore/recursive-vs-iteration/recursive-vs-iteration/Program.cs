65ab1e7197659b32153ace7916db04ca66479b56
Wenyang Yao <wyao@concordservicing.com>
2019-10-09 16:28:00 -0700

    Added src for c# and F#

---
+++ src/csharpcore/recursive-vs-iteration/recursive-vs-iteration/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace recursive_vs_iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch1 = new Stopwatch();
+            Stopwatch stopWatch2 = new Stopwatch();
+
+            string filepath = @"result.csv";
+            using (StreamWriter sw = new StreamWriter(filepath, false))
+            {
+                string header = $"Index,Recursion,Interative";
+                sw.WriteLine(header);
+                for (int n = 0; n <= 50; n++)
+                {
+                    stopWatch1.Start();
+                    long result = GetFibonacciNumberRecursively(n);
+                    stopWatch1.Stop();
+                    stopWatch2.Start();
+                    long result2 = GetFibonacciNumberIteratively(n);
+                    stopWatch2.Stop();
+
+                    long ts1 = stopWatch1.ElapsedMilliseconds;
+                    long ts2 = stopWatch2.ElapsedMilliseconds;
+
+                    string row = $"{n},{ts1},{ts2}";
+                    sw.WriteLine(row);
+
+                    Console.Write($"n={n}".PadLeft(15).PadRight(15));
+                    Console.Write($"{ts1}".PadLeft(15).PadRight(15));
+                    Console.Write($"{ts2}".PadLeft(15).PadRight(15));
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static long GetFibonacciNumberRecursively(int index)
+        {
+            if (index == 0)
+                return 0;
+            if (index == 1)
+                return 1;
+            return GetFibonacciNumberRecursively(index - 1) + GetFibonacciNumberRecursively(index - 2);
+        }
+
+        static long GetFibonacciNumberIteratively(int index)
+        {
+            long sum = 0;
+            if (index == 0)
+                return 0;
+            if (index == 1)
+                return 1;
+            long f0 = 0;
+            long f1 = 1;
+            for(int i = 2; i <= index; i++)
+            {
+                sum = f0 + f1;
+                f0 = f1;
+                f1 = sum;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ src/fsharpcpre/recursion-vs-iteration/recursion-vs-iteration/Program.fs
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec getFibonacciNumber(index:int64) = 
+    if index = 0L then
+        0L
+    else if index = 1L then
+        1L
+    else
+        getFibonacciNumber(index - 1L) + getFibonacciNumber(index - 2L) 
+
+let getFibonacciNumberIterative(index:int64) = 
+    if index = 0L then
+        0L
+    else if index = 1L then
+        1L
+    else
+        let mutable fn:int64 = 0L
+        let mutable f0:int64 = 0L
+        let mutable f1:int64 = 1L
+        for i in 2L .. index do
+            fn <- f0 + f1
+            f0 <- f1
+            f1 <- fn
+        fn
+      
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    Console.WriteLine(getFibonacciNumberIterative(50L))
+    0 // return an integer exit code
+
